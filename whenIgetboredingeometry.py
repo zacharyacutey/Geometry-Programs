66e0d389d0fc0e979dd160f7ed3a9c5d1e490f3b
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-28 11:03:15 -0500

    Update whenIgetboredingeometry.py
    Garrett, or's don't work like that!

--- whenIgetboredingeometry.py
+++ whenIgetboredingeometry.py
@@ -1,6 +1,6 @@
 import math
 import cmath
-
+solution=None
 
 def distance():
 	#user input
@@ -22,11 +22,10 @@ def law_of_cosines():
 	#has a bug though
 	while True:
 		is_user_solving_for_side_or_angle = input("Are you solving for a \n1. Side\n2. Angle\n\nAnswer: ")
-		if is_user_solving_for_side_or_angle == 1 | 2:
+		if is_user_solving_for_side_or_angle == "1" or is_user_solving_for_side_or_angle == "2":
 			break
 		else:
 			print("\n\nI don't quite understand what you're trying to tell me.  Could you type that again?\n\n")
-			continue
 
 	if is_user_solving_for_side_or_angle == "1":
 		a = int(input("A: "))
@@ -67,17 +66,14 @@ def quadform():
 while True:
 	which_formula = int(input("Which formula would you like to use?\n\n1. Distance Formula\n2. Quadratic formula\n3. Law of Cosines\n\n"))
 
-	if which_formula == 1 or 2 or 3:
+	if which_formula == 1 or which_formula==2 or which_formula==3:
 		break
 	else:
 		print("\n\nI don't quite understand what you're trying to tell me.  Could you type that again?\n\n")
-
+print("\n")
 if which_formula == 1:
-	print("\n")
 	distance()
 elif which_formula == 2:
-	print("\n")
 	quadform()
 elif which_formula == 3:
-	print("\n")
 	law_of_cosines()
